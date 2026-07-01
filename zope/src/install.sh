5686888bf3e437bbfdf2ef37f0ce2f5052ca0285
Benoît Pin <benoit.pin@gmail.com>
2026-07-01 21:04:03 +0200

    install script

--- zope/src/install.sh
+++ zope/src/install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+mkdir -p "$ZOPE_HOME/zeo_instances" "$ZOPE_HOME/zope_instances"
+
+mkzeoinstance "$ZOPE_HOME/zeo_instances/plinn"
+mv "$ZOPE_HOME/zeo.conf" "$ZOPE_HOME/zeo_instances/plinn/etc/zeo.conf"
+
+mkzopeinstance -d "$ZOPE_HOME/zope_instances/plinn" -u admin:admin
+mv "$ZOPE_HOME/zope.conf" "$ZOPE_HOME/zope_instances/plinn/etc/zope.conf"
+
+exit 0
 #
 # Buildout config file to use. Default: base.cfg
 #
